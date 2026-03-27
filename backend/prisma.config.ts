0722db2485a060e87d958d98e34b88a203bd080e
horspid <horspid@yandex.ru>
2026-03-27 07:35:45 +0500

    added Docker configuration

---
+++ backend/prisma.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "prisma/config";
+
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+
+dotenvExpand.expand(dotenv.config())
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: { 
+    path: "prisma/migrations" 
+  },
+  datasource: { 
+    url: process.env.POSTGRES_URI 
+  },
+});
