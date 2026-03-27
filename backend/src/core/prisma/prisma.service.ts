d46a5d4237de0ac5af8a7e43f8c5242540e48eb1
horspid <horspid@yandex.ru>
2026-03-27 20:04:16 +0500

    updated Prisma settings

--- backend/src/core/prisma/prisma.service.ts
+++ backend/src/core/prisma/prisma.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaClient } from 'prisma/generated/prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+    super({ adapter });
+  }
+}
