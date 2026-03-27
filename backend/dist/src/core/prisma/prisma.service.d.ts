c20d537f7f8fc5cbf64223b8aa93b62bb8fd3bdf
horspid <horspid@yandex.ru>
2026-03-27 20:25:49 +0500

    added sync with backend and db

--- backend/dist/src/core/prisma/prisma.service.d.ts
+++ backend/dist/src/core/prisma/prisma.service.d.ts
@@ -1,2 +1,7 @@
-export declare class PrismaService {
+import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'prisma/generated/prisma/client';
+export declare class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor();
+    onModuleInit(): Promise<void>;
+    onModuleDestroy(): Promise<void>;
 }
--- backend/src/core/prisma/prisma.service.ts
+++ backend/src/core/prisma/prisma.service.ts
@@ -1,12 +1,23 @@
 
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from 'prisma/generated/prisma/client';
+import { PrismaPg } from "@prisma/adapter-pg";
+
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  constructor() {
-    const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
-    super({ adapter });
-  }
-}
+
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+        const prisma = new PrismaClient({ adapter });
+        
+        super({ adapter })
+    }
+    async onModuleInit() {
+        await this.$connect()
+    }
+    async onModuleDestroy() {
+        await this.$disconnect()
+    }
+}    
+ 
